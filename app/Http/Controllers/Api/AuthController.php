ebd9f02fad89629f4c4684e93adce94c8472a0f9
VigoMade <vigovigo129@gmail.com>
2024-05-27 08:36:39 +0700

    update la sikit

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -15,94 +15,95 @@ use Illuminate\Validation\Rule;
 class AuthController extends Controller
 {
     public function login(Request $request)
-{
-    $credentials = $request->only('username', 'password');
+    {
+        $credentials = $request->only('username', 'password');
 
-    // Validasi data masukan
-    $validator = Validator::make($credentials, [
-        'username' => 'required|string',
-        'password' => 'required|string',
-    ]);
-
-    // Jika validasi gagal, kembalikan respon dengan status 422 (Unprocessable Entity)
-    if ($validator->fails()) {
-        return response()->json(['message' => $validator->errors()], 422);
-    }
-
-    // Lakukan autentikasi pegawai
-    if (auth()->guard('pegawai')->attempt($credentials)) {
-        $pegawai = auth()->guard('pegawai')->user();
-        $token = $pegawai->createToken('Personal Access Token')->accessToken;
-
-        // Cek peran pegawai
-        switch ($pegawai->jabatan->role) {
-            case 'Admin':
-                return response()->json([
-                    'message' => 'Berhasil terautentikasi sebagai Pemilik',
-                    'user' => $pegawai,
-                    'id_customer' => $pegawai->id_pegawai,
-                    'role' => $pegawai->jabatan->role,
-                    'email' => $pegawai->email_pegawai,
-                    'noTelpon' => $pegawai->telepon_pegawai,
-                    'nama' => $pegawai->nama_pegawai,
-                    'image' => $pegawai->foto,
-                    'token' => $token,
-                ]);
-                break;
-            case 'Owner':
-                return response()->json([
-                    'message' => 'Berhasil terautentikasi sebagai Admin',
-                    'user' => $pegawai,
-                    'id_customer' => $pegawai->id_pegawai,
-                    'role' => $pegawai->jabatan->role,
-                    'email' => $pegawai->email_pegawai,
-                    'noTelpon' => $pegawai->telepon_pegawai,
-                    'nama' => $pegawai->nama_pegawai,
-                    'image' => $pegawai->foto,
-                    'token' => $token,
-                ]);
-                break;
-            case 'MO':
-                return response()->json([
-                    'message' => 'Berhasil terautentikasi sebagai MO',
-                    'user' => $pegawai,
-                    'id_customer' => $pegawai->id_pegawai,
-                    'role' => $pegawai->jabatan->role,
-                    'email' => $pegawai->email_pegawai,
-                    'noTelpon' => $pegawai->telepon_pegawai,
-                    'nama' => $pegawai->nama_pegawai,
-                    'image' => $pegawai->foto,
-                    'token' => $token,
-                ]);
-                break;
-            default:
-                return response()->json(['message' => 'Tidak diizinkan'], 401);
-                break;
-        }
-    }
-
-    // Lakukan autentikasi pelanggan
-    if (auth()->guard('customer')->attempt($credentials)) {
-        $customer = auth()->guard('customer')->user();
-        $token = $customer->createToken('Personal Access Token')->accessToken;
-
-        // Tentukan respons sesuai peran pelanggan
-        return response()->json([
-            'message' => 'Berhasil terautentikasi sebagai Pelanggan',
-            'user' => $customer,
-            'id_customer' => $customer->id_customer,
-            'role' => 'Customer',
-            'email' => $customer->email,
-            'noTelpon' => $customer->noTelpon,
-            'token' => $token,
-            'nama' => $customer->nama,
-            'image' => $customer->image ? $customer->image : "",
+        // Validasi data masukan
+        $validator = Validator::make($credentials, [
+            'username' => 'required|string',
+            'password' => 'required|string',
         ]);
-    }
 
-    // Jika autentikasi gagal, kembalikan respon Unauthorized
-    return response()->json(['message' => 'Tidak diizinkan'], 401);
-}
+        // Jika validasi gagal, kembalikan respon dengan status 422 (Unprocessable Entity)
+        if ($validator->fails()) {
+            return response()->json(['message' => $validator->errors()], 422);
+        }
+
+        // Lakukan autentikasi pegawai
+        if (auth()->guard('pegawai')->attempt($credentials)) {
+            $pegawai = auth()->guard('pegawai')->user();
+
+            $token = $pegawai->createToken('Personal Access Token')->accessToken;
+
+            // Cek peran pegawai
+            switch ($pegawai->jabatan->role) {
+                case 'Admin':
+                    return response()->json([
+                        'message' => 'Berhasil terautentikasi sebagai Pemilik',
+                        'user' => $pegawai,
+                        'id_customer' => $pegawai->id_pegawai,
+                        'role' => $pegawai->jabatan->role,
+                        'email' => $pegawai->email_pegawai,
+                        'noTelpon' => $pegawai->telepon_pegawai,
+                        'nama' => $pegawai->nama_pegawai,
+                        'image' => $pegawai->foto,
+                        'token' => $token,
+                    ]);
+                    break;
+                case 'Owner':
+                    return response()->json([
+                        'message' => 'Berhasil terautentikasi sebagai Admin',
+                        'user' => $pegawai,
+                        'id_customer' => $pegawai->id_pegawai,
+                        'role' => $pegawai->jabatan->role,
+                        'email' => $pegawai->email_pegawai,
+                        'noTelpon' => $pegawai->telepon_pegawai,
+                        'nama' => $pegawai->nama_pegawai,
+                        'image' => $pegawai->foto,
+                        'token' => $token,
+                    ]);
+                    break;
+                case 'MO':
+                    return response()->json([
+                        'message' => 'Berhasil terautentikasi sebagai MO',
+                        'user' => $pegawai,
+                        'id_customer' => $pegawai->id_pegawai,
+                        'role' => $pegawai->jabatan->role,
+                        'email' => $pegawai->email_pegawai,
+                        'noTelpon' => $pegawai->telepon_pegawai,
+                        'nama' => $pegawai->nama_pegawai,
+                        'image' => $pegawai->foto,
+                        'token' => $token,
+                    ]);
+                    break;
+                default:
+                    return response()->json(['message' => 'Tidak diizinkan'], 401);
+                    break;
+            }
+        }
+
+        // Lakukan autentikasi pelanggan
+        if (auth()->guard('customer')->attempt($credentials)) {
+            $customer = auth()->guard('customer')->user();
+            $token = $customer->createToken('Personal Access Token')->accessToken;
+
+            // Tentukan respons sesuai peran pelanggan
+            return response()->json([
+                'message' => 'Berhasil terautentikasi sebagai Pelanggan',
+                'user' => $customer,
+                'id_customer' => $customer->id_customer,
+                'role' => 'Customer',
+                'email' => $customer->email,
+                'noTelpon' => $customer->noTelpon,
+                'token' => $token,
+                'nama' => $customer->nama,
+                'image' => $customer->image ? $customer->image : "",
+            ]);
+        }
+
+        // Jika autentikasi gagal, kembalikan respon Unauthorized
+        return response()->json(['message' => 'Tidak diizinkan'], 401);
+    }
 
 
 
@@ -159,7 +160,7 @@ class AuthController extends Controller
     //     if (auth()->guard('customer')->attempt($credentials)) {
     //         $customer = auth()->guard('customer')->user();
     //         $token = $customer->createToken('Personal Access Token')->accessToken;
-            
+
     //         // Tentukan respons sesuai peran customer
     //         return response()->json([
     //             'message' => 'Authenticated as Customer',
--- app/Http/Controllers/Api/HistoriController.php
+++ app/Http/Controllers/Api/HistoriController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Api;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth; // Perlu tambahkan ini untuk menggunakan Auth
 use App\Models\Transaksi;
@@ -15,33 +16,35 @@ class HistoriController extends Controller
     {
         $userId = Auth::guard('customer')->id();
         $transaksi = Transaksi::with('produk', 'penitip')
-                        ->where(function($query) use ($id) {
-                            $query->where('id_customer', $id)
-                                  ->orWhere('id_pegawai', $id);
-                        })
-                        ->get();
-                        
+            ->where(function ($query) use ($id) {
+                $query->where('id_customer', $id)
+                    ->orWhere('id_pegawai', $id);
+            })
+            ->where(function ($query) {
+                $query->where('status', 'Selesai')
+                    ->orWhere('status', 'DiTolak');
+            })
+            ->get();
         if ($transaksi->isNotEmpty()) {
             $transaksiData = $transaksi->map(function ($item) {
-                if($item->id_penitip_fk){
+                if ($item->id_penitip_fk) {
                     return [
                         'id_transaksi' => $item->id_transaksi,
                         'id_customer' => $item->id_customer,
-                        'image' => $item->penitip->image,
-                        'nama_produk' => $item->penitip->nama_produk_penitip,
+                        'image' => $item->penitip->image ?? "",
+                        'nama_produk' => $item->penitip->nama_produk_penitip ?? "",
                         'status' => $item->status,
                     ];
-                }else{
+                } else {
                     return [
                         'id_transaksi' => $item->id_transaksi,
                         'id_customer' => $item->id_customer,
-                        'image' => $item->produk->image,
-                        'nama_produk' => $item->produk->nama_produk,
+                        'image' => $item->produk->image ?? "",
+                        'nama_produk' => $item->produk->nama_produk ?? "",
                         'status' => $item->status,
                     ];
                 }
-                
-            });            
+            });
 
             return response([
                 'message' => 'Retrieve All Success',
@@ -53,51 +56,49 @@ class HistoriController extends Controller
             'message' => 'Empty',
             'data' => null
         ], 400);
-        
     }
 
     public function search(String $request)
-{
-    $search = $request;
+    {
+        $search = $request;
 
-    if (!$search) {
-        return response()->json([
-            'error' => 'Query parameter is missing',
-        ], 400);
-    }
-
-    try {
-        $transactions = Transaksi::whereHas('produk', function ($query) use ($search) {
-            $query->where('namaProduk', 'like', "%$search%");
-        })->orWhereHas('produk.penitip', function ($query) use ($search) {
-            $query->where('namaProdukPenitip', 'like', "%$search%");
-        })->with('produk')->get();
-
-        if ($transactions->isEmpty()) {
+        if (!$search) {
             return response()->json([
-                'message' => 'No matching records found',
-                'data' => [],
-            ], 200);
+                'error' => 'Query parameter is missing',
+            ], 400);
         }
 
-        $transaksiData = $transactions->map(function ($item) {
-            return [
-                'id_transaksi' => $item->id_transaksi, // Assuming this is the correct field name for the transaction ID
-                'image' => $item->produk->image,
-                'status' => $item->status,
-                'nama_produk' => $item->produk->namaProduk,
-            ];
-        });
+        try {
+            $transactions = Transaksi::whereHas('produk', function ($query) use ($search) {
+                $query->where('namaProduk', 'like', "%$search%");
+            })->orWhereHas('produk.penitip', function ($query) use ($search) {
+                $query->where('namaProdukPenitip', 'like', "%$search%");
+            })->with('produk')->get();
 
-        return response()->json([
-            'message' => 'Retrieve Success',
-            'data' => $transaksiData,
-        ], 200);
-    } catch (Exception $e) {
-        return response()->json([
-            'error' => 'Internal Server Error',
-        ], 500);
+            if ($transactions->isEmpty()) {
+                return response()->json([
+                    'message' => 'No matching records found',
+                    'data' => [],
+                ], 200);
+            }
+
+            $transaksiData = $transactions->map(function ($item) {
+                return [
+                    'id_transaksi' => $item->id_transaksi, // Assuming this is the correct field name for the transaction ID
+                    'image' => $item->produk->image,
+                    'status' => $item->status,
+                    'nama_produk' => $item->produk->namaProduk,
+                ];
+            });
+
+            return response()->json([
+                'message' => 'Retrieve Success',
+                'data' => $transaksiData,
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => 'Internal Server Error',
+            ], 500);
+        }
     }
-}
-
 }
